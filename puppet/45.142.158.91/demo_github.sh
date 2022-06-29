1585824524398ad97d0a28f85254a12907046b41
姚博海 <86307090+yaobohai@users.noreply.github.com>
2022-06-29 16:50:30 +0800

    Update demo_github.sh

--- puppet/45.142.158.91/demo_github.sh
+++ puppet/45.142.158.91/demo_github.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 text="hello,world123"
-https://github.com/yaobohai/base_mirrors/blob/develop/puppet/45.142.158.91/demo_github.sh
+
 function main() {
   echo $text > /tmp/demo_github.sh
 }
