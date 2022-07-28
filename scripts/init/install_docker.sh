a13e42a51e6ce8403d476d45e9041c0d28dac0b0
姚博海 <86307090+yaobohai@users.noreply.github.com>
2022-07-28 17:32:16 +0800

    Update install_docker.sh

--- scripts/init/install_docker.sh
+++ scripts/init/install_docker.sh
@@ -22,14 +22,3 @@ EOF
 
 ## 重启docker服务
 systemctl  daemon-reload && systemctl restart docker
-
-# 部署下载程序
-docker rm -f remote_download
-docker run -itd \
--p 8999:80 \
---restart=always \
---name=remote_download \
--e PASSWORD=123456 \
--e SERVER_NAME=$(curl -4s ip.sb):8999 \
--v /data/remote_download:/app/remote_download/files \
-registry.cn-hangzhou.aliyuncs.com/bohai_repo/remote_download:v1.0
