595c1dc9aa72ef2433c2bfcccaca2fbb45f0f991
姚波海 <yaobohai@hd123.com>
2022-12-11 18:27:00 +0800

    add register_linux monitor for consul

--- scripts/monitor/prometheus/register_linux.sh
+++ scripts/monitor/prometheus/register_linux.sh
@@ -4,7 +4,8 @@ idc=$1
 region=$2
 monitor_center_server=$3
 hostname=$(hostname|cut -d'.' -f1)
-if [ ${monitor_center_server} == '' ];then monitor_center_server='42.192.186.124:8500';fi
+os_address_external=$(curl -4s ip.sb)
+if [[ $monitor_center_server == '' ]];then monitor_center_server='42.192.186.124:8500';fi
 
 docker ps &>/dev/null
 if [ $? != 0 ]; then echo "[ERROR] host: ${hostname} docker not runing";exit 1;fi
@@ -28,3 +29,5 @@ curl -XPUT -d \
 	"tags": ["linux"],
 	"checks": [{"http": "'"http://${os_address_external}:9100/metrics"'",
 	"interval": "35s","timeout": "60s"}]}' http://${monitor_center_server}/v1/agent/service/register
+
+unlink $0
