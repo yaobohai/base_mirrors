7be09d4b0348c47cf00cd5e29353f91a38a0d899
姚博海 <86307090+yaobohai@users.noreply.github.com>
2023-11-15 00:03:41 +0800

    Update register_linux_Intranet.sh

--- scripts/monitor/prometheus/register_linux_Intranet.sh
+++ scripts/monitor/prometheus/register_linux_Intranet.sh
@@ -6,29 +6,30 @@ idc='本地云'
 region='中国-上海市'
 exporter_path='/app'
 exporter_version='1.6.1'
-monitor_center_server=$3
-hostname=$(hostname|cut -d'.' -f1)
-os_address_external=$(hostname -I|awk '{print $1}')
-if [[ $monitor_center_server == '' ]];then monitor_center_server='42.192.186.124:8500';fi
+host_name=$(hostname|cut -d'.' -f1)
+host_addr=$(hostname -I|awk '{print $1}')
+consul_server='42.192.186.124:8500'
 
 docker rm -f node-exporter
-rm -rf  ${exporter_path}/node_exporter
-mkdir -p ${exporter_path}
-curl -so /tmp/node_exporter-v${exporter_version}.tar.gz \
-https://mirrors.itan90.cn/scripts/monitor/prometheus/resouce/node_exporter-v${exporter_version}.tar.gz \
-&& tar zxvf /tmp/node_exporter-v${exporter_version}.tar.gz -C ${exporter_path} \
-&& mv ${exporter_path}/node_exporter/node_exporter.service /etc/systemd/system/ \
-&& systemctl daemon-reload \
-&& systemctl enable node_exporter --now
+
+if [[ ! -d ${exporter_path}/node_exporter ]];then
+	mkdir -p ${exporter_path}
+	curl -so /tmp/node_exporter-v${exporter_version}.tar.gz \
+	https://mirrors.itan90.cn/scripts/monitor/prometheus/resouce/node_exporter-v${exporter_version}.tar.gz \
+	&& tar zxvf /tmp/node_exporter-v${exporter_version}.tar.gz -C ${exporter_path} \
+	&& mv ${exporter_path}/node_exporter/node_exporter.service /etc/systemd/system/ \
+	&& systemctl daemon-reload \
+	&& systemctl enable node_exporter --now
+fi
 
 curl -XPUT -d \
-	'{"id": "'"${hostname}-${os_address_external}"'",
+	'{"id": "'"${hostname}"'",
 	"name": "linux-internal",
-	"address": "'"${os_address_external}"'",
+	"address": "'"${hostaddr}"'",
 	"port": 9100,"Meta": {"country": "'"${region}"'",
 	"idc": "'"${idc}"'",
 		"app": "'"${hostname}"'",
-	"ip": "'"${os_address_external}"'"},
+	"ip": "'"${hostaddr}"'"},
 	"tags": ["linux-internal"],
-	"checks": [{"http": "'"http://${os_address_external}:9100/metrics"'",
-	"interval": "35s","timeout": "60s"}]}' http://${monitor_center_server}/v1/agent/service/register
+	"checks": [{"http": "'"http://${hostaddr}:9100/metrics"'",
+	"interval": "35s","timeout": "60s"}]}' http://${consul_server}/v1/agent/service/register
