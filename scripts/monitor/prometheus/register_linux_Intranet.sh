0d7f9bcec3a2022fe40a25840d89bca594289d2a
姚博海 <86307090+yaobohai@users.noreply.github.com>
2023-11-16 11:44:28 +0800

    Update register_linux_Intranet.sh

--- scripts/monitor/prometheus/register_linux_Intranet.sh
+++ scripts/monitor/prometheus/register_linux_Intranet.sh
@@ -10,26 +10,26 @@ host_name=$(hostname|cut -d'.' -f1)
 host_addr=$(hostname -I|awk '{print $1}')
 consul_server='42.192.186.124:8500'
 
-docker rm -f node-exporter
+docker rm -f node-exporter &>/dev/null
 
 if [[ ! -d ${exporter_path}/node_exporter ]];then
-	mkdir -p ${exporter_path}
-	curl -so /tmp/node_exporter-v${exporter_version}.tar.gz \
-	https://mirrors.itan90.cn/scripts/monitor/prometheus/resouce/node_exporter-v${exporter_version}.tar.gz \
-	&& tar zxvf /tmp/node_exporter-v${exporter_version}.tar.gz -C ${exporter_path} \
-	&& mv ${exporter_path}/node_exporter/node_exporter.service /etc/systemd/system/ \
-	&& systemctl daemon-reload \
-	&& systemctl enable node_exporter --now
+        mkdir -p ${exporter_path}
+        curl -so /tmp/node_exporter-v${exporter_version}.tar.gz \
+        https://mirrors.itan90.cn/scripts/monitor/prometheus/resouce/node_exporter-v${exporter_version}.tar.gz \
+        && tar zxvf /tmp/node_exporter-v${exporter_version}.tar.gz -C ${exporter_path} \
+        && mv ${exporter_path}/node_exporter/node_exporter.service /etc/systemd/system/ \
+        && systemctl daemon-reload \
+        && systemctl enable node_exporter --now
 fi
 
 curl -XPUT -d \
-	'{"id": "'"${hostname}"'",
-	"name": "linux-internal",
-	"address": "'"${hostaddr}"'",
-	"port": 9100,"Meta": {"country": "'"${region}"'",
-	"idc": "'"${idc}"'",
-		"app": "'"${hostname}"'",
-	"ip": "'"${hostaddr}"'"},
-	"tags": ["linux-internal"],
-	"checks": [{"http": "'"http://${hostaddr}:9100/metrics"'",
-	"interval": "35s","timeout": "60s"}]}' http://${consul_server}/v1/agent/service/register
+        '{"id": "'"${host_name}"'",
+        "name": "linux-internal",
+        "address": "'"${host_addr}"'",
+        "port": 9100,"Meta": {"country": "'"${region}"'",
+        "idc": "'"${idc}"'",
+        "app": "'"${host_name}"'",
+        "ip": "'"${host_addr}"'"},
+        "tags": ["linux-internal"],
+        "checks": [{"http": "'"http://${host_addr}:9100/metrics"'",
+        "interval": "35s","timeout": "60s"}]}' http://${consul_server}/v1/agent/service/register
